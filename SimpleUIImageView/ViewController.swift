df7b5eb07040b26fef09fb4138d86341ac5bca26
Sunny NG <ngsanluk@gmail.com>
2017-02-21 16:18:01 +0800

    First commit

--- SimpleUIImageView/ViewController.swift
+++ SimpleUIImageView/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // outlet connecting stroyboard's UIImageView
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    @IBAction func showPicture(_ sender: Any) {
+        
+        // Loading image from UIImage constructor taking file name as input
+        imageView.image = UIImage(named: "logan")
+        
+    }
+    
+    @IBAction func imageViewTapped(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
